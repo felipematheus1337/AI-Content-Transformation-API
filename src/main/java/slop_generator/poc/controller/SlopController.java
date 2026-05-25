06994e569f2c3afb8b6cc0baf3f1fbb1520dc5b5
felipematheus1337 <lipehbr@gmail.com>
2026-05-25 11:46:01 -0300

    Created app.yml with configuration to OpenAI, Created SlopController, with all 4 parameters.

---
+++ src/main/java/slop_generator/poc/controller/SlopController.java
@@ -0,0 +1,25 @@
+package slop_generator.poc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SlopController {
+
+    @GetMapping("/tweets")
+    public String tweets(
+            @RequestParam(name = "originalTweet", required = false,
+                    defaultValue = "How to learn how to program fast") String originalTweet,
+            @RequestParam(name = "topicHint", required = false,
+                    defaultValue = "Spring AI") String topicHint,
+            @RequestParam(name = "emojiLevel", required = false,
+                    defaultValue = "low") String emojiLevel,
+            @RequestParam(name = "modernizationLevel", required = false,
+                    defaultValue = "extreme") String modernizationLevel) {
+
+
+        return "200";
+    }
+}
+
