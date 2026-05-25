872f5366029bc61569557092a7f9dc2ef4362812
felipematheus1337 <lipehbr@gmail.com>
2026-05-25 11:27:14 -0300

    Project created.

---
+++ src/test/java/slop_generator/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package slop_generator.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
